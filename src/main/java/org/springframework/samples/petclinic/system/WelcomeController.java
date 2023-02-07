001229d1fe360fcb49990786612fd37f3ec2699b
Sumit Deo <sumitdeo1988@gmail.com>
2023-02-06 19:14:41 -0800

    Add logging

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -16,14 +16,23 @@
 
 package org.springframework.samples.petclinic.system;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class WelcomeController {
 
+	Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
 	@GetMapping("/")
 	public String welcome() {
+		logger.trace("A TRACE Message");
+		logger.debug("A DEBUG Message");
+		logger.info("An INFO Message");
+		logger.warn("A WARN Message");
+		logger.error("An ERROR Message");
 		return "welcome";
 	}
 
